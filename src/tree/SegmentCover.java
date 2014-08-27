9dcc3cd4993adede2a1741ec43ff32acf3fd1cc7
luxury <luxr123@foxmail.com>
2014-08-27 16:10:58 +0800

    update

--- src/tree/SegmentCover.java
+++ src/tree/SegmentCover.java
@@ -19,7 +19,7 @@ public class SegmentCover {
 	class Node {// 线段树
 		int left;
 		int right;
-		int isCover; // 标记是否被覆盖
+		boolean isCover; // 标记是否被覆盖
 	}
 
 	public SegmentCover() {
@@ -32,22 +32,22 @@ public class SegmentCover {
 		segTree[index].left = lef;
 		segTree[index].right = rig;
 		if (rig - 1 == lef) {// 到单位1线段
-			segTree[index].isCover = 0;
+			segTree[index].isCover = false;
 			return;
 		}
 		int mid = (lef + rig) >> 1;
 		construct((index << 1) + 1, lef, mid);
 		construct((index << 1) + 2, mid, rig); // 非mid+1，线段覆盖[mid,mid+1]
-		segTree[index].isCover = 0;
+		segTree[index].isCover = false;
 	}
 
 	/* 插入线段[start,end]到线段树, 同时标记覆盖 */
 	void insert(int index, int start, int end) {
-		if (segTree[index].isCover == 1)
+		if (segTree[index].isCover)
 			return; // 如已覆盖，没必要继续向下插
 
 		if (segTree[index].left == start && segTree[index].right == end) {
-			segTree[index].isCover = 1;
+			segTree[index].isCover = true;
 			return;
 		}
 		int mid = (segTree[index].left + segTree[index].right) >> 1;
@@ -64,7 +64,7 @@ public class SegmentCover {
 
 	/* 计算线段覆盖长度 */
 	int count(int index) {
-		if (segTree[index].isCover == 1) {
+		if (segTree[index].isCover) {
 			return segTree[index].right - segTree[index].left;
 		} else if (segTree[index].right - segTree[index].left == 1) {
 			return 0;
@@ -83,10 +83,10 @@ public class SegmentCover {
 			cover.insert(0, segment[i][0], segment[i][1]);
 		}
 		System.out.format("the cover length is %d\n", cover.count(0));
-		
-		int a=192,b=168,c=1,d=100;
-		System.out.println((a<<24)|(b<<16)|(c<<8)|d);
-		System.out.println((a<<20)|(b<<12)|(c<<4)|d);
+
+		int a = 192, b = 168, c = 1, d = 100;
+		System.out.println((a << 24) | (b << 16) | (c << 8) | d);
+		System.out.println((a << 20) | (b << 12) | (c << 4) | d);
 	}
 
 }
